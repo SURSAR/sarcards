d172678e3a346179ac962185a0924ddcd2180e38
Barnaby <barnaby@wirehive.net>
2015-11-25 19:46:28 +0000

    details

--- models/member.py
+++ models/member.py
@@ -4,11 +4,75 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 from web import db
 
-class Member(db.Model):
+class GlobalQualification(db.Model):
     """"""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    image = db.Column(db.String(255))
-
     def __str__(self):
         return self.title
+
+class GlobalRole(db.Model):
+    """"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    def __str__(self):
+        return self.title
+
+class Team(db.Model):
+    """"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    logo = db.Column(db.String(255), nullable=False)
+    charity_number = db.Column(db.String(255))
+    address = db.Column(db.String(4000), nullable=False)
+    def __str__(self):
+        return self.name
+
+class TeamQualification(db.Model):
+    """"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    team_id =  db.Column(db.Integer, db.ForeignKey(Team.id), nullable=False)
+    team = db.relationship(Team, backref='qualifications')
+    global_id = db.Column(db.Integer, db.ForeignKey(GlobalQualification.id))
+    global_qualification = db.relationship(GlobalQualification, backref='team_qualifications')
+    def __str__(self):
+        return self.title
+
+class TeamRole(db.Model):
+    """"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    team_id =  db.Column(db.Integer, db.ForeignKey(Team.id), nullable=False)
+    team = db.relationship(Team, backref='roles')
+    global_id = db.Column(db.Integer, db.ForeignKey(GlobalRole.id))
+    global_role = db.relationship(GlobalRole, backref='team_roles')
+    def __str__(self):
+        return self.title
+
+class Member(db.Model):
+    """"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255))
+    team_id = db.Column(db.Integer, db.ForeignKey(Team.id))
+    team = db.relationship(Team, backref='members')
+    qualifications = db.relationship(TeamQualification, secondary='member_team_qualification', backref='members')
+    roles = db.relationship(TeamRole, secondary='member_team_role', backref='members')
+    def __str__(self):
+        return self.name
+
+#pylint: disable=invalid-name
+MemberTeamQualification = db.Table(
+    'member_team_qualification',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('member_id',db.Integer, db.ForeignKey(Member.id)),
+    db.Column('qualification_id',db.Integer, db.ForeignKey(TeamQualification.id)),
+)
+
+MemberTeamRole = db.Table(
+    'member_team_role',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('member_id',db.Integer, db.ForeignKey(Member.id)),
+    db.Column('role_id',db.Integer, db.ForeignKey(TeamRole.id)),
+)
--- web/member.py
+++ web/member.py
@@ -9,7 +9,7 @@ from flask_admin import form
 from jinja2 import Markup
 
 from . import admin, db
-from models.member import Member
+from models.member import *
 
 class MemberView(ModelView):
     """"""
@@ -31,5 +31,32 @@ class MemberView(ModelView):
             thumbnail_size=(100, 100, True)
         )
     }
+
+class TeamView(ModelView):
+    """"""
+    def _list_thumbnail(self, context, model, name):
+        """"""
+        if not model.logo:
+            return ''
+        return Markup('<img src="%s">' % url_for(
+            'static',
+            filename=form.thumbgen_filename(model.logo)
+        ))
+    column_formatters = {
+        'logo': _list_thumbnail
+    }
+    form_extra_fields = {
+        'logo': form.ImageUploadField(
+            'Logo',
+            base_path='static',
+            thumbnail_size=(100, 100, True)
+        )
+    }
+
+admin.add_view(ModelView(GlobalQualification, db.session))
+admin.add_view(ModelView(GlobalRole, db.session))
+admin.add_view(TeamView(Team, db.session))
+admin.add_view(ModelView(TeamQualification, db.session))
+admin.add_view(ModelView(TeamRole, db.session))
 
 admin.add_view(MemberView(Member, db.session))
